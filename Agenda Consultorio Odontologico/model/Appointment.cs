35919d3f4d6df4d3becd79ccfd727a741ba07630
silveiraluisf <silveiraluis88@gmail.com>
2022-12-14 11:25:31 -0300

    criação da classe Appointment, junto com a primeira estrutura da interface e controladores do agendamento de consulta

---
+++ Agenda Consultorio Odontologico/model/Appointment.cs
@@ -0,0 +1,28 @@
+﻿namespace Agenda_Consultorio_Odontologico.model
+{
+    public class Appointment
+    {
+        private DateTime _Date;
+        private int _start;
+        private int _end;
+        private Patient _patient;
+        public DateTime Date
+        {
+            get { return _Date; }
+            set { _Date = value; }
+        }
+        public int Start
+        {
+            get { return _start; }
+            set { _start = value; }
+        }
+        public int End
+        { 
+            get { return _end; } 
+            set { _end = value; } 
+        }
+        public Patient Patient { 
+            get { return this._patient; }
+        }
+    }    
+}
---
+++ Agenda Consultorio Odontologico/view/AppointmentRegistrationInterface.cs
@@ -0,0 +1,64 @@
+﻿namespace Agenda_Consultorio_Odontologico.view
+{
+    public class AppointmentRegistrationInterface
+    {
+        private string _inputPatientCPF;
+        private string _inputDate;
+        private string _inputStart;
+        private string _inputEnd;
+        public string InputPatientCPF
+        {
+            get { return _inputPatientCPF; }
+            set { _inputPatientCPF = value; }
+        }
+        public string InputDate
+        {
+            get { return _inputDate; }
+            set { _inputDate = value; }
+        }
+        public string InputStart
+        {
+            get { return _inputStart; }
+            set { _inputStart = value; }
+        }
+        public string InputEnd
+        {
+            get { return _inputEnd; }
+            set { _inputEnd = value; }
+        }
+        public void Title()
+        {
+            Console.WriteLine("Agendamento de consulta \n");
+        }
+        public void GetPatientCPF()
+        {
+            Console.WriteLine("Insira o CPF do paciente: ");
+            this._inputPatientCPF = Console.ReadLine();
+        }
+        public void GetDate()
+        {
+            Console.WriteLine("Insira a data da consulta (dd/mm/aaaa): ");
+            this._inputDate = Console.ReadLine();
+        }
+        public void GetStart()
+        {
+            Console.WriteLine("Insira o horário de início da consulta (HHMM): ");
+            this._inputStart = Console.ReadLine();
+        }
+        public void GetEnd()
+        {
+            Console.WriteLine("Insira o horário de término da consulta (HHMM): ");
+            this._inputEnd = Console.ReadLine();
+        }
+        public void ShowData()
+        {
+            Console.WriteLine($"--------------------------------------------");
+            Console.WriteLine($"CPF: {this.InputPatientCPF}");
+            Console.WriteLine($"Data da consulta: {this.InputDate}");
+            Console.WriteLine($"Hora inicial: {this.InputStart} \n");
+            Console.WriteLine($"Hora final: {this.InputEnd} \n");
+            Console.WriteLine("Agendamento realizado com sucesso!\n");
+
+        }
+    }
+}
